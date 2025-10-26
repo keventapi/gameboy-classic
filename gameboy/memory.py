50b7103238e190eed59f19473f5d71c2fb029c37
keven <92277070+keventapi@users.noreply.github.com>
2025-10-26 18:33:19 -0300

    criado vram e ram

--- gameboy/memory.py
+++ gameboy/memory.py
@@ -6,11 +6,32 @@ class Ram:
         if 0xC000 <= addrs < 0xE000:
             offset = addrs - 0xC000
             self.memory[offset] = value
-    
+        elif 0xE000 <= addrs < 0xFE00:
+            offset = addrs - 0xE000
+            self.memory[offset] = value
+
     def read(self, addrs):
-        return self.memory[addrs]
+        if 0xC000 <= addrs < 0xE000:
+            offset = addrs - 0xC000
+            return self.memory[offset]
+        elif 0xE000 <= addrs < 0xFE00:
+            offset = addrs - 0xE000
+            return self.memory[offset]
+        return None
     
+
 class Vram:
     def __init__(self, size=0x2000):
         self.memory = [0]*size 
+    
+    def write(self, addrs, value):
+        if 0x8000 <= addrs < 0xA000:
+            offset = addrs - 0x8000
+            self.memory[offset] = value
+    
+    def read(self, addrs):
+        if 0x8000 <= addrs < 0xA000:
+            offset = addrs - 0x8000
+            return self.memory[offset]
+        return None
     
