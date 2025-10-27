59c81e4c81b66af3acc8051e219fa36c2c24308b
keven <92277070+keventapi@users.noreply.github.com>
2025-10-27 16:53:12 -0300

    Add files via upload

---
+++ gameboy/memory/MBC.py
@@ -0,0 +1,127 @@
+class MBC:
+    def __init__(self, ram, rom, mbc_version):
+        self.ram = ram 
+        self.rom = rom
+        self.ram_enabled = False
+        self.mode = 0
+        self.mbc_version = mbc_version
+        self.mbc5_lower = 0
+        self.mbc5_upper = 0 
+        self.rom_low_bank = None
+
+
+    def select_rom_bank(self, bank):
+        if bank == 0:
+            bank = 1
+        self.rom_low_bank = bank
+        self.rom.switch_bank(bank)
+
+    def handle_write(self, addrs, value):
+        if self.mbc_version == 1:
+            self.write_mbc1(addrs, value)
+        elif self.mbc_version == 2:
+            self.write_mbc2(addrs, value)
+        elif self.mbc_version == 3:
+            self.write_mbc3(addrs, value)
+        elif self.mbc_version == 5:
+            self.write_mbc5(addrs, value)
+        return 0xFF
+
+    def handle_read(self, addrs):
+        if self.mbc_version == 1:
+            return self.read_mbc1(addrs)
+        elif self.mbc_version == 2:
+            return self.read_mbc2(addrs)
+        elif self.mbc_version == 3:
+            return self.read_mbc3(addrs)
+        elif self.mbc_version == 5:
+            return self.read_mbc5(addrs)
+        return 0xFF
+
+    def write_mbc1(self, addrs, value):
+        if 0xA000 <= addrs < 0xC000:
+            if self.ram_enabled:
+                self.ram.write(addrs, value)
+
+        if 0x0000 <= addrs < 0x2000:
+            self.ram_enabled = (value & 0b1111) == 0b1010
+
+        if 0x2000 <= addrs < 0x4000:
+            bank = value & 0x1F
+            self.select_rom_bank(bank)
+
+        if 0x6000 <= addrs < 0x8000:
+            self.mode = value & 1
+            if self.mode == 0:
+                self.ram.switch_bank(0)
+
+        if self.mode == 1:
+            if 0x4000 <= addrs < 0x6000:
+                bank = value & 0b11
+                self.ram.switch_bank(bank)
+
+        elif self.mode == 0:
+            if 0x4000 <= addrs < 0x6000:
+                high_bank = value & 0b11 
+                bank = (high_bank << 5) | self.rom_low_bank
+                self.rom.switch_bank(bank)
+
+    def write_mbc2(self, addrs, value):
+        if 0x0000 <= addrs < 0x1FFF and (addrs & 0x0100) == 0:
+            self.ram_enabled = not self.ram_enabled
+
+        elif 0x2000<= addrs < 0x4000 and (addrs & 0x0100) != 0:
+            bank = value & 0x0F
+            self.select_rom_bank(bank)
+
+        if 0xA000 <= addrs < 0xC000:
+            if self.ram_enabled:
+                self.ram.write(addrs, value)
+
+    def write_mbc3(self, addrs, value):
+        if 0xA000 <= addrs < 0xC000:
+            if self.ram_enabled:
+                self.ram.write(addrs, value)
+
+        
+        if 0x4000 <= addrs < 0x6000:
+            return #precisa da implementação do rtc (clock)
+        
+        if 0x0000 <= addrs < 0x2000:
+            self.ram_enabled = (value & 0b1111) == 0b1010
+
+        if 0x2000 <= addrs < 0x4000:
+            self.select_rom_bank(value & 0b01111111)
+ 
+    def write_mbc5(self, addrs, value):
+        if 0x2000 <= addrs < 0x3000:
+            self.mbc5_lower = value & 0xFF
+        if 0x3000 <= addrs < 0x4000:
+            self.mbc5_upper = value & 0b1
+            bank = (self.mbc5_upper << 8) | self.mbc5_lower
+            self.select_rom_bank(bank)
+        if 0x4000 <= addrs < 0x6000 and self.ram is not None and self.ram_enabled:
+            bank = value & 0xFF
+            self.ram.switch_bank(bank)
+        
+        if 0xA000 <= addrs < 0xC000 and self.ram_enabled and self.ram is not None:
+            self.ram.write(addrs, value)
+
+            
+    def read_mbc1(self, addrs):
+        if 0xA000 <= addrs < 0xC000:
+            if self.ram_enabled and self.ram is not None:
+                return self.ram.read(addrs)
+            return 0xFF
+        
+        if 0x0000 <= addrs < 0x8000:
+            return self.rom.read(addrs)
+    
+    def read_mbc2(self, addrs):
+        return self.read_mbc1(addrs)
+    
+    def read_mbc3(self, addrs):
+        return self.read_mbc1(addrs)
+
+    def read_mbc5(self, addrs):
+        return self.read_mbc1(addrs)
