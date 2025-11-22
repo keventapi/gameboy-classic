fd8df8dd640f317e1b87f8508fc43d398364b2ae
keventapi <lohankeven983@outlook.com>
2025-11-22 16:06:32 -0300

    adcionado push e pop do stack

---
+++ gameboy/cpus/instroctions/stack/pop.py
@@ -0,0 +1,21 @@
+class POP:
+    def __init__(self):
+        pass
+
+    def pop_instructions(self):
+        instructions = {
+            0xF1: lambda: self.execute_pop("AF"),
+            0xC1: lambda: self.execute_pop("BC"),
+            0xD1: lambda: self.execute_pop("DE"),
+            0xE1: lambda: self.execute_pop("HL")
+        }
+        return instructions
+    
+    def execute_pop(self, r16):
+        high = r16[0]
+        low = r16[1]
+        if low == "F":
+            self.registers[low] = self.pull8() & 0xF0
+        else:
+            self.registers[low] = self.pull8()
+        self.registers[high] = self.pull8()
