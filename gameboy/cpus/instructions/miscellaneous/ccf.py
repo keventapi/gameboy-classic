b84b483d4a4997df1ecf5ae2f4f5cfc892d75dbc
keventapi <lohankeven983@outlook.com>
2025-12-11 01:41:32 -0300

    feature --> CCF instructions

---
+++ gameboy/cpus/instructions/miscellaneous/ccf.py
@@ -0,0 +1,19 @@
+class CCF:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.registers = self.cpu.registers
+
+    def instructions_ccf(self):
+        instructions = {
+            0x3F: lambda: self.execute_ccf(4)
+        }
+        return instructions
+
+    def execute_ccf(self, ticks):
+        flag = self.registers["F"]
+        Z = (flag >> 7) & 1
+        N = 0
+        H = 0
+        C = (flag >> 4) ^ 1
+        self.cpu.set_flags(Z, N, H, C)
+        self.cpu.timer.tick(ticks)
