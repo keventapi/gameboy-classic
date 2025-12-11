5a1eae2cdef5f116da6806aa3cff4159f295a616
keventapi <lohankeven983@outlook.com>
2025-12-11 01:50:51 -0300

    feature --> NOP instruction

---
+++ gameboy/cpus/instructions/miscellaneous/nop.py
@@ -0,0 +1,12 @@
+class NOP:
+    def __init__(self, cpu):
+        self.cpu = cpu
+
+    def intructions_nop(self):
+        instructions = {
+            0x00: lambda: self.execute_nop(4)
+        }
+        return instructions
+
+    def execute_nop(self, ticks):
+        self.cpu.timer.tick(ticks)
