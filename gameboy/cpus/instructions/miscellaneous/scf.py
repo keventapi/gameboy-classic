3811bc740d894a81494b81cc5ef418c9860e7ac2
keventapi <lohankeven983@outlook.com>
2025-12-11 01:46:52 -0300

    feature --> scf instruction

---
+++ gameboy/cpus/instructions/miscellaneous/scf.py
@@ -0,0 +1,19 @@
+class SCF:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.registers = self.cpu.registers
+
+    def instructions_scf(self):
+        instructions = {
+            0x37: lambda: self.execute_scf(4)
+        }
+        return instructions
+
+    def execute_scf(self, ticks):
+        flag = self.registers["F"]
+        Z = (flag >> 7) & 1
+        N = 0
+        H = 0
+        C = 1
+        self.cpu.set_flags(Z, N, H, C)
+        self.cpu.timer.tick(ticks)
