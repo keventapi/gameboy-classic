042b661c405f475068a76cd03ada37e8ef55bbf8
keventapi <lohankeven983@outlook.com>
2025-12-13 20:30:35 -0300

    feature --> call incondictional and condictional

---
+++ gameboy/cpus/instructions/calls/call_cc_nn.py
@@ -0,0 +1,37 @@
+class CALL_CC_NN:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.registers = self.cpu.registers
+        self.mmu = self.cpu.mmu
+
+    def call_cc_nn_instructions(self):
+        instructions = {
+            0xC4: lambda: self.execute_call_cc_nn(12),
+            0xCC: lambda: self.execute_call_cc_nn(12),
+            0xD4: lambda: self.execute_call_cc_nn(12),
+            0xDC: lambda: self.execute_call_cc_nn(12)
+        }
+        return instructions
+
+    def execute_call_cc_nn(self, condiction, ticks):
+        addrs = self.cpu.fetch16()
+        pc = self.registers["PC"]
+
+        high = (pc >> 8) & 0xFF
+        low = pc & 0xFF
+
+        flags = self.registers["F"]
+        if "Z" in condiction:
+            value = (flags >> 7) & 1
+        else:
+            value = (flags >> 4) & 1
+
+        condiction_met = (value == 1) if "N" not in condiction else value == 0
+        if condiction_met:
+            self.cpu.push8(high)
+            self.cpu.push8(low)
+
+            self.registers["pc"] = addrs
+            self.cpu.timer.tick(ticks*2)
+            return
+        self.cpu.timer.tick(ticks)
---
+++ gameboy/cpus/instructions/calls/call_nn.py
@@ -0,0 +1,24 @@
+class CALL_NN:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.registers = self.cpu.registers
+        self.mmu = self.cpu.mmu
+
+    def call_nn_instructions(self):
+        instructions = {
+            0xCD: lambda: self.execute_call_nn(24)
+        }
+        return instructions
+
+    def execute_call_nn(self, ticks):
+        addrs = self.cpu.fetch16()
+        pc = self.registers["PC"]
+
+        high = (pc >> 8) & 0xFF
+        low = pc & 0xFF
+
+        self.cpu.push8(high)
+        self.cpu.push8(low)
+
+        self.registers["pc"] = addrs
+        self.cpu.timer.tick(ticks)
