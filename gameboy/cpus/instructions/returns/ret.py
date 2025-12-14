1e80555073b85dd9cacc06b1cc0764c9cfb0969f
keventapi <lohankeven983@outlook.com>
2025-12-14 02:31:08 -0300

    feature --> return and interrupt instruction set

--- gameboy/cpus/instructions/returns/ret.py
+++ gameboy/cpus/instructions/returns/ret.py
@@ -6,14 +6,17 @@ class RET:
 
     def ret_instructions(self):
         instructions = {
-            0xC9: lambda: self.execute_ret(16)
+            0xC9: lambda: self.execute_ret(False, 16),
+            0xD9: lambda: self.execute_ret(True, 16)
         }
         return instructions
 
-    def execute_ret(self, ticks):
+    def execute_ret(self, enable_interrupt, ticks):
         low = self.cpu.pull8()
         high = self.cpu.pull8()
         new_pc = (high << 8) | low
 
         self.registers["pc"] = new_pc
+        if enable_interrupt:
+            self.cpu.timer.interrupt = True
         self.cpu.timer.tick(ticks)
