be3a1a69a2c1e079f2fcdd1567112b3e1df4e275
keventapi <lohankeven983@outlook.com>
2025-12-14 00:22:27 -0300

    feature --> incondictional return

---
+++ gameboy/cpus/instructions/returns/ret.py
@@ -0,0 +1,19 @@
+class RET:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.registers = self.cpu.registers
+        self.mmu = self.cpu.mmu
+
+    def ret_instructions(self):
+        instructions = {
+            0xC9: lambda: self.execute_ret(16)
+        }
+        return instructions
+
+    def execute_ret(self, ticks):
+        low = self.cpu.pull8()
+        high = self.cpu.pull8()
+        new_pc = (high << 8) | low
+
+        self.registers["pc"] = new_pc
+        self.cpu.timer.tick(ticks)
