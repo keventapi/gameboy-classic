0eff4aa50f04a882c393be3f0da0e3e9706244ee
keventapi <lohankeven983@outlook.com>
2026-01-31 12:37:50 -0300

    feature --> stop instruction \n perfomace refactoring --> flag to make the reset on ppu state be only at the change of lcd mode to 0, lambdas was changed to declareted functions using a code generator made by myself

--- gameboy/cpus/instructions/miscellaneous/daa.py
+++ gameboy/cpus/instructions/miscellaneous/daa.py
@@ -16,28 +16,28 @@ class DAA:
         old_H = (flag >> 5) & 1
         old_C = (flag >> 4) & 1
         C = old_C
-
+        new_value = value 
         if old_N == 0:
             if old_H or (value & 0x0F) > 0x09:
-                value += 0x06
+                new_value += 0x06
 
             if old_C or value > 0x99:
-                value += 0x60
+                new_value += 0x60
                 C = 1
 
         else:
             if old_C:
-                value -= 0x60
+                new_value -= 0x60
 
             if old_H:
-                value -= 0x06
+                new_value -= 0x06
 
-        value &= 0xFF
-        Z = 1 if value == 0 else 0
+        new_value &= 0xFF
+        Z = 1 if new_value == 0 else 0
         N = old_N
         H = 0
 
         self.cpu.set_flags(Z, N, H, C)
-        self.registers["A"] = value & 0xFF
+        self.registers["A"] = new_value & 0xFF
         self.cpu.timer.tick(ticks)
         return ticks
